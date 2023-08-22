b83a45a3ae61d04b169103a4bad77d6bcd40e8b7
Tas de Foin <douzmaur@gmail.com>
2023-08-22 18:50:18 +0200

    Custom admin page

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from.models import AppUser, Sport, UserSports, FeedBack
 
-# Register your models here.
+
+
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    search_fields = ['username', 'email']
+
+    def username(self, au):
+        return au.user_id.username
+    
+    def email(self, au):
+        return au.user_id.email
+    
+
+
+@admin.register(Sport)
+class SportAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+
+@admin.register(UserSports)
+class UserSportsAdmin(admin.ModelAdmin):
+    list_display = ['user', 'sport']
+    search_fields = ['user']
+    list_filter = ['sport_id']
+
+    def user(self, us):
+        return us.user_id.username
+    
+    def sport(self, us):
+        return us.sport_id.name
+
+
+
+@admin.register(FeedBack)
+class FeedBackAdmin(admin.ModelAdmin):
+    list_display = ['user', 'raison_choice', 'attente_choice']
+    search_fields = ['user']
+    list_filter = ['raison_choice', 'attente_choice']
+
+
+    def user(self, fb):
+        return fb.user_id.username
