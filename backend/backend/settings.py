ba8a91edb0737d3031f610f44bb3f60288dcbaf6
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-09-22 21:54:54 +0200

    models : post + comment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'user_api',
+    'post_api',
 ]
 
 MIDDLEWARE = [
--- backend/post_api/models.py
+++ backend/post_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user_model = get_user_model()
+
+class Post(models.Model):
+    post_author = models.ForeignKey(user_model, related_name="post_author", on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    desc = models.TextField()
+    time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    posted_time = models.DateTimeField(auto_now_add=True, editable=False)
+    location = models.CharField(max_length=128)
+    private = models.BooleanField()
+    nb_limit = models.SmallIntegerField()
+    inscrits = models.ManyToManyField(user_model, related_name="inscrits", blank=True)
+
+
+class Comments(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(user_model, related_name="comment_author", on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class 
